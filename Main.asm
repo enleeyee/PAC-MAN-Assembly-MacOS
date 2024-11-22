2b5dc77a6c2ce2d0d9d1d8ca0560ce7ebd3aba73
Enlai <enleyee@gmail.com>
2024-11-22 16:28:36 -0600

    added graphics

--- Main.asm
+++ Main.asm
@@ -4,6 +4,27 @@ MODEL small
 stack 600h
 DATASEG
 ;{
+    ;Clock & Timers {
+		CLOCK_BASE 	DB 0
+	
+		INT_MOV		DW 3
+		CNT_MOV		DW 0 
+		
+		INT_GMOV	DW 3
+		CNT_GMOV	DW 0
+		
+		INT_MODE	DW 180
+		CNT_MODE	DW 0
+		INT_CHASE	DW 126
+		INT_SCAT	DW 72
+		
+		INT_FRI		DW 126
+		CNT_FRI		DW 0
+		
+		INT_BLINK	DW 5
+		CNT_BLINK	DW 0
+	;}
+
 	;MAIN MENU {
 		MENU_PTR	DB 0
 		STR_PLAY	DB "PLAY$"
@@ -26,11 +47,18 @@ CODESEG
 ;{
 	;PROCEDURES
 	;{	
+        INCLUDE "Graphics.asm"
 		INCLUDE "MainMenu.asm"
+        INCLUDE "Time.asm"
+		INCLUDE "Timers.asm"
 	;}
 
 	;CODE:
 		Start:
+
+        ;INIT{ 		
+			call graphics_Mode
+		;}
 		
 		CALL MAINMENU
 		
@@ -44,6 +72,8 @@ CODESEG
 		Update:
 		;{
 			CALL GAME_INPUT	
+
+            CALL CLOCK
 		;}
 		JMP  Update			
 	
