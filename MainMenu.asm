d17f6859a1cbf92cf264862463738e5f3e164c32
Enlai <enleyee@gmail.com>
2024-11-23 19:21:11 -0600

    Refactor MainMenu.asm to NASM

--- MainMenu.asm
+++ MainMenu.asm
@@ -1,281 +1,262 @@
-PROC MENU_INPUT
-;{
-	PUSH AX
-	PUSH BX
-	
-	;CODE 
-	;{
-		;NEW DATA? {
-			MOV AH, 1
-			INT 16H
-			
-			JNZ @@NEWDATA 	;NEW DATA
-			JMP	@@END_PROC	;ELSE, NO DATA
-		;}
-		
-		;HANDLE NEW DATA {
-			@@NEWDATA:
-			;CLEAR BUFFER FROM DATA
-			MOV AH, 0
-			INT 16H
-	
-			;PROCESS NEW DATA
-				
-				;DATA = UP ARROW?
-				CMP AH, ARR_U
-				JZ  @@UP
-				
-				;DATA = DOWN ARROW?
-				CMP AH, ARR_D
-				JZ  @@DOWN
-				
-				;DATA = SPACEBAR?
-				CMP AH, SC_SPACE
-				JZ  @@CLICK
-				
-				;DATA = ENTER?
-				CMP AH, SC_ENTER
-				JZ  @@CLICK
-				JMP @@END_PROC
+section .data
+    ARR_U db 0x48     
+    ARR_D db 0x50          
+    SC_SPACE db 0x20      
+    SC_ENTER db 0x0D     
+    RED db 0x04         
+    WHITE db 0x07        
+    MENU_PTR dw 0       
+    MENU_TXT_COL db 0      
 
-		;}
-		
-		;SET NEW DIRECTION {
-			@@CLICK: ;{
-				CALL MENU_CLICK
-				JMP  @@END_PROC
-			;}
-			
-			@@UP: ;{
-				DEC [MENU_PTR]
-				JMP @@OVERFLOW
-			;}
-			
-			@@DOWN: ;{
-				INC [MENU_PTR]
-			;}		
-		;}
-	
-	@@OVERFLOW: ;{
-		CMP [MENU_PTR], 3
-		JZ  @@MIN_PTR
-		
-		;CMP [MENU_PTR], 0FFFFh
-		JNZ @@COLOR
-		
-		@@MAX_PTR: ;{
-			MOV [MENU_PTR], 2
-			JMP @@COLOR
-		;}
-		
-		@@MIN_PTR: ;{
-			MOV [MENU_PTR], 0
-		;}	
-	;}
-	
-	@@COLOR: ;{
-		MOV BX, OFFSET MENU_TXT_COL
-		MOV [WORD PTR BX]    , WHITE
-		MOV [WORD PTR BX + 2], WHITE
-		MOV [WORD PTR BX + 4], WHITE
-		
-		MOV BL, [MENU_PTR]
-		XOR BH, BH
-		SHL BX, 1
-		ADD BX, OFFSET MENU_TXT_COL
-		MOV [WORD PTR BX], RED 
-	;}
+section .text
+global _start
 
-	@@END_PROC:
-	;FLUSH BUFFER {
-		XOR AL, AL
-		MOV AH, 0CH
-		INT 21H
-		
-		POP BX
-		POP AX
-		RET
-	;}
-;}
-ENDP MENU_INPUT
+MENU_INPUT:
+    push ax
+    push bx
 
-PROC MENU_PRINT_TITLE
-;{
-	;PRINT BASE {
-		PUSH 113
-		PUSH 40
-		PUSH 95
-		PUSH 20
-		PUSH DARK_RED
-		CALL GRAPHICS_PRINTRECT
-		
-		PUSH 116
-		PUSH 43
-		PUSH 89
-		PUSH 14
-		PUSH 29h
-		CALL GRAPHICS_PRINTRECT
-	
-	;}
-	
-	;PRINT 'P' {
-		PUSH OFFSET LETTER_P
-		PUSH YELLOW
-		PUSH 120
-		PUSH 45
-		CALL GRAPHICS_PRINTIMAGE
-	;}
-	
-	;PRINT 'A' {
-		PUSH OFFSET LETTER_A
-		PUSH YELLOW
-		PUSH 132
-		PUSH 45
-		CALL GRAPHICS_PRINTIMAGE
-	;}
-	
-	;PRINT 'C' {
-		PUSH OFFSET PACMAN_R_2
-		PUSH YELLOW
-		PUSH 144
-		PUSH 45
-		CALL GRAPHICS_PRINTIMAGE
-	;}
-	
-	;PRINT '-' {
-		PUSH 157
-		PUSH 48
-		PUSH 7
-		PUSH 3
-		PUSH YELLOW
-		CALL GRAPHICS_PRINTRECT
-	;}
-	
-	;PRINT 'M' {
-		PUSH OFFSET LETTER_M
-		PUSH YELLOW
-		PUSH 168
-		PUSH 45
-		CALL GRAPHICS_PRINTIMAGE
-	;}
-		
-	;PRINT 'A' {
-		PUSH OFFSET LETTER_A
-		PUSH YELLOW
-		PUSH 180
-		PUSH 45
-		CALL GRAPHICS_PRINTIMAGE
-	;}	
-	
-	;PRINT 'N' {
-		PUSH OFFSET LETTER_N
-		PUSH YELLOW
-		PUSH 192
-		PUSH 45
-		CALL GRAPHICS_PRINTIMAGE
-	;}
-	
-	RET
-;}
-ENDP MENU_PRINT_TITLE
+    mov ah, 1
+    int 0x16             
+    jnz NEWDATA        
+    jmp END_PROC          
+NEWDATA:
+    mov ah, 0
+    int 0x16              
 
-PROC MENU_PRINT_TEXT
-;{
-	;PUSH REGS {
-		PUSH AX
-		PUSH BX
-		PUSH SI
-		PUSH DI
-	;}
-	
-	;CODE {
-		;18 = CUR_X, AX = CUR_Y
-		MOV AX, 15
-		
-		MOV SI, OFFSET MENU_TXT_OFF
-		MOV DI, OFFSET MENU_TXT_COL
-		XOR BX, BX
-				
-		@@TEXT_LOOP:
-		;{
-			PUSH 18
-			PUSH AX
-			CALL TEXT_SET_CURPOS
-			
-			PUSH [DI + BX]
-			PUSH [SI + BX]
-			CALL TEXT_COLORSTR
-			
-			ADD AX, 2
-			ADD BX, 2
-			CMP BX, 6
-			JNZ @@TEXT_LOOP
-		;}
-		
-	;}
-	
-	@@END_PROC: ;{
-		POP DI
-		POP SI
-		POP BX
-		POP AX
-	;}
-	RET
-;}
-ENDP MENU_PRINT_TEXT
+    cmp ah, [ARR_U]        
+    jz UP                  
+    
+    cmp ah, [ARR_D]        
+    jz DOWN                 
+    
+    cmp ah, [SC_SPACE]     
+    jz CLICK                
+    
+    cmp ah, [SC_ENTER]     
+    jz CLICK                
+    jmp END_PROC           
 
-PROC MENU_CLICK
-;{
-	CMP [MENU_PTR], BTN_PLAY
-	JZ  @@PLAY
-	
-	JMP EXIT
-	
-	@@PLAY: ;{
-		CALL NEW_GAME
-	;}
-	
-	@@END_PROC:	
-	RET
-;}
-ENDP MENU_CLICK
+CLICK:
+    call MENU_CLICK
+    jmp END_PROC
 
-PROC MAINMENU
-;{
-	;MENU INIT {
-		MOV BX, OFFSET MENU_TXT_OFF
-		MOV [WORD PTR BX]    , OFFSET STR_PLAY
-		MOV [WORD PTR BX + 4], OFFSET STR_EXIT
-		
-		MOV BX, OFFSET MENU_TXT_COL
-		MOV [WORD PTR BX]    , RED
-		MOV [WORD PTR BX + 2], WHITE
-		MOV [WORD PTR BX + 4], WHITE
-	;}
-	
-	;PRINT THE TITLE {
-		PUSH 0
-		PUSH 0
-		PUSH 320
-		PUSH 200
-		PUSH 68h
-		CALL GRAPHICS_PRINTRECT
-		
-		PUSH 85
-		PUSH 25
-		PUSH 150
-		PUSH 150
-		PUSH BLACK
-		CALL GRAPHICS_PRINTRECT
-		
-		CALL MENU_PRINT_TITLE
-	;}
-	
-	@@MAINMENU: ;{
-		CALL MENU_INPUT
-		CALL MENU_PRINT_TEXT
-		JMP  @@MAINMENU
-	;}
-	RET
-;}
-ENDP MAINMENU
+UP:
+    dec word [MENU_PTR]    
+    jmp OVERFLOW           
+
+DOWN:
+    inc word [MENU_PTR]    
+    jmp END_PROC
+
+OVERFLOW:
+    cmp word [MENU_PTR], 3
+    jz MIN_PTR
+    cmp word [MENU_PTR], 0
+    jnz COLOR
+
+MAX_PTR:
+    mov word [MENU_PTR], 2
+    jmp COLOR
+
+MIN_PTR:
+    mov word [MENU_PTR], 0
+
+COLOR:
+    mov bx, MENU_TXT_COL
+    mov word [bx], WHITE
+    mov word [bx + 2], WHITE
+    mov word [bx + 4], WHITE
+
+    mov bl, [MENU_PTR]
+    xor bh, bh
+    shl bx, 1
+    add bx, MENU_TXT_COL
+    mov word [bx], RED
+
+END_PROC:
+    xor al, al
+    mov ah, 0x0C
+    int 0x21               
+
+    pop bx
+    pop ax
+    ret
+
+MENU_PRINT_TITLE:
+    push 113
+    push 40
+    push 95
+    push 20
+    push DARK_RED
+    call GRAPHICS_PRINTRECT
+
+    push 116
+    push 43
+    push 89
+    push 14
+    push 29h
+    call GRAPHICS_PRINTRECT
+
+    ; Print 'P'
+    push LETTER_P
+    push YELLOW
+    push 120
+    push 45
+    call GRAPHICS_PRINTIMAGE
+
+    ; Print 'A'
+    push LETTER_A
+    push YELLOW
+    push 132
+    push 45
+    call GRAPHICS_PRINTIMAGE
+
+    ; Print 'C'
+    push PACMAN_R_2
+    push YELLOW
+    push 144
+    push 45
+    call GRAPHICS_PRINTIMAGE
+
+    ; Print '-'
+    push 157
+    push 48
+    push 7
+    push 3
+    push YELLOW
+    call GRAPHICS_PRINTRECT
+
+    ; Print 'M'
+    push LETTER_M
+    push YELLOW
+    push 168
+    push 45
+    call GRAPHICS_PRINTIMAGE
+
+    ; Print 'A'
+    push LETTER_A
+    push YELLOW
+    push 180
+    push 45
+    call GRAPHICS_PRINTIMAGE
+
+    ; Print 'N'
+    push LETTER_N
+    push YELLOW
+    push 192
+    push 45
+    call GRAPHICS_PRINTIMAGE
+
+    ret
+
+MENU_PRINT_TEXT:
+    push ax
+    push bx
+    push si
+    push di
+
+    mov ax, 15             
+    mov si, MENU_TXT_OFF
+    mov di, MENU_TXT_COL
+    xor bx, bx
+
+TEXT_LOOP:
+    push 18
+    push ax
+    call TEXT_SET_CURPOS
+
+    push [di + bx]
+    push [si + bx]
+    call TEXT_COLORSTR
+
+    add ax, 2
+    add bx, 2
+    cmp bx, 6
+    jnz TEXT_LOOP
+
+    pop di
+    pop si
+    pop bx
+    pop ax
+    ret
+
+MENU_CLICK:
+    cmp word [MENU_PTR], BTN_PLAY
+    jz PLAY
+
+    cmp word [MENU_PTR], BTN_HELP
+    jz HELP
+    jmp EXIT
+
+PLAY:
+    call NEW_GAME
+    ret
+
+HELP:
+    push 14
+    push 9
+    call TEXT_SET_CURPOS
+
+    push STR_INST_1
+    call TEXT_PRINT_MSG
+
+    push 14
+    push 10
+    call TEXT_SET_CURPOS
+
+    push STR_INST_2
+    call TEXT_PRINT_MSG
+
+    push 15
+    push 11
+    call TEXT_SET_CURPOS
+
+    push STR_INST_3
+    call TEXT_PRINT_MSG
+
+    mov ah, 7
+    int 0x21              
+
+    push 112
+    push 72
+    push 104
+    push 24
+    push BLACK
+    call GRAPHICS_PRINTRECT
+    ret
+
+MAINMENU:
+    ; Menu initialization
+    mov bx, MENU_TXT_OFF
+    mov word [bx], STR_PLAY
+    mov word [bx + 2], STR_HELP
+    mov word [bx + 4], STR_EXIT
+
+    mov bx, MENU_TXT_COL
+    mov word [bx], RED
+    mov word [bx + 2], WHITE
+    mov word [bx + 4], WHITE
+
+    ; Print the title
+    push 0
+    push 0
+    push 320
+    push 200
+    push 68h
+    call GRAPHICS_PRINTRECT
+
+    push 85
+    push 25
+    push 150
+    push 150
+    push BLACK
+    call GRAPHICS_PRINTRECT
+
+    call MENU_PRINT_TITLE
+
+MAINMENU_LOOP:
+    call MENU_INPUT
+    call MENU_PRINT_TEXT
+    jmp MAINMENU_LOOP
+
+    ret
