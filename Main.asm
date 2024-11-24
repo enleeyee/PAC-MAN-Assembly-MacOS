bbb2aff71cdc126c312fecd6118edf11347e747a
Enlai <enleyee@gmail.com>
2024-11-23 23:20:36 -0600

    final refactor main.asm to NASM

--- Main.asm
+++ Main.asm
@@ -1,231 +1,164 @@
-.286
-IDEAL
-MODEL small
-stack 600h
-DATASEG
-;{
-    ;Clock & Timers {
-		CLOCK_BASE 	DB 0
-	
-		INT_MOV		DW 3
-		CNT_MOV		DW 0 
-		
-		INT_GMOV	DW 3
-		CNT_GMOV	DW 0
-		
-		INT_MODE	DW 180
-		CNT_MODE	DW 0
-		INT_CHASE	DW 126
-		INT_SCAT	DW 72
-		
-		INT_FRI		DW 126
-		CNT_FRI		DW 0
-		
-		INT_BLINK	DW 5
-		CNT_BLINK	DW 0
-	;}
+section .data
+    ; Clock & Timers
+    CLOCK_BASE db 0
 
-    ;Genearl (Levels and difficulty) {
-		SPEED   		DW 4
-		DUR_1ST_SCAT	DW 108
-		DUR_CHASE		DW 540
-		DUR_SCAT		DW 72
-		DUR_FRI			DW 54
-		LEVEL			DW 0
-	;}
+    INT_MOV dw 3
+    CNT_MOV dw 0
 
-    ;Ghosts {
-		Ghosts 		DW 56 dup (0)
-		ARR_END 	DW 0
-		ARR_JMP		DW 28
-		IS_FRIGH	DB FALSE
-		EATGHOST	DW 200
-		PREV_MODE	DW MODE_SCAT
-		G_BLINK		DW -1
-		G_FRI_COL	DW 86h, 44h, WHITE_1, RED
-	;}
+    INT_GMOV dw 3
+    CNT_GMOV dw 0
 
-    ;Images & Animations {
-		INCLUDE "Images.asm"
-		PAC_ANI_U	DW 8 DUP (0)
-		PAC_ANI_D	DW 8 DUP (0)
-		PAC_ANI_L	DW 8 DUP (0)
-		PAC_ANI_R	DW 8 DUP (0)		
-		PAC_FP		DW 0	
+    INT_MODE dw 180
+    CNT_MODE dw 0
+    INT_CHASE dw 126
+    INT_SCAT dw 72
 
-		LOSE_ANI	DW 6 DUP (0)
-	;}
+    INT_FRI dw 126
+    CNT_FRI dw 0
 
-    ;Layout & Dots {
-		CNT_DOTS 		DW 174 ;TOTAL DOTS = 180dots + 4pps
-		CNT_DOTS_TEMP	DW 0
-	;}
+    INT_BLINK dw 5
+    CNT_BLINK dw 0
 
-	;MAIN MENU {
-		MENU_PTR	DB 0
-		STR_PLAY	DB "PLAY$"
-		STR_EXIT	DB "EXIT$"
-		STR_INST_1	DB "ARROWS = MOVE$"
-		STR_INST_3	DB " DOTS = EAT$"
-		STR_INST_2	DB "GHOSTS = BAD$"
-	
-		MENU_TXT_OFF	DW 0,0,0
-		MENU_TXT_COL	DW 0,0,0
-	;}
+    ; General (Levels and difficulty)
+    SPEED dw 4
+    DUR_1ST_SCAT dw 108
+    DUR_CHASE dw 540
+    DUR_SCAT dw 72
+    DUR_FRI dw 54
+    LEVEL dw 0
 
-    ;PacMan Vars {
-		PACX			DW 99
-		PACY			DW 144 
-		DIR				DW DIR_N
-		NEXTDIR			DW DIR_N
-		LIVES			DW 4
-	;}
+    ; Ghosts
+    Ghosts dw 56 dup (0)
+    ARR_END dw 0
+    ARR_JMP dw 28
+    IS_FRIGH db 0
+    EATGHOST dw 200
+    PREV_MODE dw 0
+    G_BLINK dw -1
+    G_FRI_COL dw 86h, 44h, 0, 4
 
-    ;UI {
-		printNumArr 	DB 0,0,0,0,'$' 
-		Print_Dec		DB 0,0,0,0,0,'$'
-		ARR_DEC			DB 0,0,0,0,0
-		SCORE			DW 0
-		HP_FLAG			DB FALSE
-		STR_GAMEOVER	DB 'GAME OVER!$'
-		STR_SCORE		DB 'SCORE:$'
-		STR_PAUSED		DB 'PAUSED$'
-		STR_PAUSED_1	DB 27h,'E', 27h, ' = EXIT$'
-		STR_PAUSED_2	DB 27h, 'R', 27h, ' = RESUME$'
-	;}
-	
-;}
+    ; Images & Animations
+    include "Images.asm"
+    PAC_ANI_U dw 8 dup(0)
+    PAC_ANI_D dw 8 dup(0)
+    PAC_ANI_L dw 8 dup(0)
+    PAC_ANI_R dw 8 dup(0)
+    PAC_FP dw 0
 
-	;{
-		INCLUDE "Colors.asm" 
-	;}
+    LOSE_ANI dw 6 dup(0)
 
-CODESEG
-;{
-	;PROCEDURES
-	;{	
-        INCLUDE "Difficulty.asm"
-        INCLUDE "Game.asm"
-        INCLUDE "Ghost.asm"
-		INCLUDE "GhostAI.asm"
-        INCLUDE "Graphics.asm"
-        INCLUDE "Layout.asm"
-		INCLUDE "MainMenu.asm"
-        INCLUDE "Math.asm" 
-        INCLUDE "Pacman.asm"
-        INCLUDE "Text.asm"
-        INCLUDE "Time.asm"
-		INCLUDE "Timers.asm"
-	;}
+    ; Layout & Dots
+    CNT_DOTS dw 174
+    CNT_DOTS_TEMP dw 0
 
-	;CODE:
-		Start:
+    ; MAIN MENU
+    MENU_PTR db 0
+    STR_PLAY db "PLAY$"
+    STR_EXIT db "EXIT$"
+    STR_INST_1 db "ARROWS = MOVE$"
+    STR_INST_3 db " DOTS = EAT$"
+    STR_INST_2 db "GHOSTS = BAD$"
 
-        ;INIT{ 		
-            ;Data:
-			MOV AX, @data
-			MOV DS, AX
-			call graphics_Mode
-		;}
+    MENU_TXT_OFF dw 0,0,0
+    MENU_TXT_COL dw 0,0,0
 
-		;INIT SPEED AND ARR_END {
-			MOV [ARR_END], OFFSET GHOSTS
-			ADD [WORD PTR ARR_END], 112		
-		;}
+    ; PacMan Vars
+    PACX dw 99
+    PACY dw 144
+    DIR dw 0
+    NEXTDIR dw 0
+    LIVES dw 4
 
-		;INIT PACMAN Animation (MOVES THE OFFSET OF THE IMAGES TO AN ARRAY) {
-			
-			;UP {
-			   ; READING THE OFFSET FORM IMAGE.ASM FILE   
-				MOV BX, OFFSET PAC_ANI_U
-				
-				MOV [WORD PTR BX], 	   	OFFSET PACMAN_0   ;incriment by 2 
-				MOV [WORD PTR BX + 2], 	OFFSET PACMAN_U_1
-				MOV [WORD PTR BX + 4], 	OFFSET PACMAN_U_2
-				MOV [WORD PTR BX + 6], 	OFFSET PACMAN_U_3
-				                                      
-				MOV [WORD PTR BX + 8],  OFFSET PACMAN_U_3
-				MOV [WORD PTR BX + 10], OFFSET PACMAN_U_2
-				MOV [WORD PTR BX + 12], OFFSET PACMAN_U_1
-				MOV [WORD PTR BX + 14], OFFSET PACMAN_0
-			;}
-			
-			;DOWN {
-				MOV BX, OFFSET PAC_ANI_D
-				
-				MOV [WORD PTR BX], 	   	OFFSET PACMAN_0
-				MOV [WORD PTR BX + 2], 	OFFSET PACMAN_D_1
-				MOV [WORD PTR BX + 4], 	OFFSET PACMAN_D_2
-				MOV [WORD PTR BX + 6], 	OFFSET PACMAN_D_3
-				                                      
-				MOV [WORD PTR BX + 8],  OFFSET PACMAN_D_3
-				MOV [WORD PTR BX + 10], OFFSET PACMAN_D_2
-				MOV [WORD PTR BX + 12], OFFSET PACMAN_D_1
-				MOV [WORD PTR BX + 14], OFFSET PACMAN_0
-			;}
-			
-			;LEFT {
-				MOV BX, OFFSET PAC_ANI_L
-				
-				MOV [WORD PTR BX], 	   	OFFSET PACMAN_0
-				MOV [WORD PTR BX + 2], 	OFFSET PACMAN_L_1
-				MOV [WORD PTR BX + 4], 	OFFSET PACMAN_L_2
-				MOV [WORD PTR BX + 6], 	OFFSET PACMAN_L_3
-				
-				MOV [WORD PTR BX + 8],  OFFSET PACMAN_L_3
-				MOV [WORD PTR BX + 10], OFFSET PACMAN_L_2
-				MOV [WORD PTR BX + 12], OFFSET PACMAN_L_1
-				MOV [WORD PTR BX + 14], OFFSET PACMAN_0
-			;}
-			
-			;RIGHT {
-				MOV BX, OFFSET PAC_ANI_R
-				
-				MOV [WORD PTR BX], 	   	OFFSET PACMAN_0
-				MOV [WORD PTR BX + 2], 	OFFSET PACMAN_R_1
-				MOV [WORD PTR BX + 4], 	OFFSET PACMAN_R_2
-				MOV [WORD PTR BX + 6], 	OFFSET PACMAN_R_3
-				
-				MOV [WORD PTR BX + 8],  OFFSET PACMAN_R_3
-				MOV [WORD PTR BX + 10], OFFSET PACMAN_R_2
-				MOV [WORD PTR BX + 12], OFFSET PACMAN_R_1
-				MOV [WORD PTR BX + 14], OFFSET PACMAN_0
-			;}
-			
-			;INIT LOSE ANIMATION {
-				MOV BX, OFFSET LOSE_ANI
-				
-				MOV [WORD PTR BX],      OFFSET PAC_LOSE_1
-				MOV [WORD PTR BX + 2],  OFFSET PAC_LOSE_2
-				MOV [WORD PTR BX + 4],  OFFSET PAC_LOSE_3
-				MOV [WORD PTR BX + 6],  OFFSET PAC_LOSE_4
-				MOV [WORD PTR BX + 8],  OFFSET PAC_LOSE_5
-				MOV [WORD PTR BX + 10], OFFSET PAC_LOSE_6
-			;}
-		;}
+    ; UI
+    printNumArr db 0,0,0,0,'$'
+    Print_Dec db 0,0,0,0,0,'$'
+    ARR_DEC db 0,0,0,0,0
+    SCORE dw 0
+    HP_FLAG db 0
+    STR_GAMEOVER db 'GAME OVER!$'
+    STR_SCORE db 'SCORE:$'
+    STR_PAUSED db 'PAUSED$'
+    STR_PAUSED_1 db 27h,'E',27h,' = EXIT$'
+    STR_PAUSED_2 db 27h,'R',27h,' = RESUME$'
 
-		CALL MAINMENU
-		
-		;WAITS UNTIL YOU CHOOSE A DIRECTION {
-			GetFirstDir:
-			CALL GAME_INPUT
-			CMP [DIR], DIR_N
-			JZ  GetFirstDir
-		;}
-		
-		Update:
-		;{
-			CALL GAME_INPUT	
-            CALL G_CHECK_DOTS
-            CALL CLOCK
-		;}
-		JMP  Update			
-	
-		Exit:
-		call text_Mode
-		
-		MOV AX, 4C00H
-		INT 21H
-		
-	END Start
-	
+section .text
+    global _start
+
+_start:
+    mov ax, 0x0
+    mov ds, ax
+    call graphics_Mode
+
+    mov [ARR_END], offset Ghosts
+    add word [ARR_END], 112
+
+    ; UP
+    mov bx, offset PAC_ANI_U
+    mov [bx], offset PACMAN_0
+    mov [bx + 2], offset PACMAN_U_1
+    mov [bx + 4], offset PACMAN_U_2
+    mov [bx + 6], offset PACMAN_U_3
+    mov [bx + 8], offset PACMAN_U_3
+    mov [bx + 10], offset PACMAN_U_2
+    mov [bx + 12], offset PACMAN_U_1
+    mov [bx + 14], offset PACMAN_0
+
+    ; DOWN
+    mov bx, offset PAC_ANI_D
+    mov [bx], offset PACMAN_0
+    mov [bx + 2], offset PACMAN_D_1
+    mov [bx + 4], offset PACMAN_D_2
+    mov [bx + 6], offset PACMAN_D_3
+    mov [bx + 8], offset PACMAN_D_3
+    mov [bx + 10], offset PACMAN_D_2
+    mov [bx + 12], offset PACMAN_D_1
+    mov [bx + 14], offset PACMAN_0
+
+    ; LEFT
+    mov bx, offset PAC_ANI_L
+    mov [bx], offset PACMAN_0
+    mov [bx + 2], offset PACMAN_L_1
+    mov [bx + 4], offset PACMAN_L_2
+    mov [bx + 6], offset PACMAN_L_3
+    mov [bx + 8], offset PACMAN_L_3
+    mov [bx + 10], offset PACMAN_L_2
+    mov [bx + 12], offset PACMAN_L_1
+    mov [bx + 14], offset PACMAN_0
+
+    ; RIGHT
+    mov bx, offset PAC_ANI_R
+    mov [bx], offset PACMAN_0
+    mov [bx + 2], offset PACMAN_R_1
+    mov [bx + 4], offset PACMAN_R_2
+    mov [bx + 6], offset PACMAN_R_3
+    mov [bx + 8], offset PACMAN_R_3
+    mov [bx + 10], offset PACMAN_R_2
+    mov [bx + 12], offset PACMAN_R_1
+    mov [bx + 14], offset PACMAN_0
+
+    ; LOSE ANIMATION
+    mov bx, offset LOSE_ANI
+    mov [bx], offset PAC_LOSE_1
+    mov [bx + 2], offset PAC_LOSE_2
+    mov [bx + 4], offset PAC_LOSE_3
+    mov [bx + 6], offset PAC_LOSE_4
+    mov [bx + 8], offset PAC_LOSE_5
+    mov [bx + 10], offset PAC_LOSE_6
+
+    call MAINMENU
+
+    GetFirstDir:
+    call GAME_INPUT
+    cmp word [DIR], 0
+    jz GetFirstDir
+
+    Update:
+    call GAME_INPUT
+    call G_CHECK_DOTS
+    call CLOCK
+    jmp Update
+
+    Exit:
+    call text_Mode
+    mov ax, 0x4C00
+    int 0x21
